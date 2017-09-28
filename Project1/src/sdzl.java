32d43be52fdad192f534e13eb5266ab3a643ca15
iAlexandreAnd <32274528+iAlexandreAnd@users.noreply.github.com>
2017-09-28 19:31:55 +0200

    Update sdzl.java

--- Project1/src/sdzl.java
+++ Project1/src/sdzl.java
@@ -5,6 +5,7 @@ public class sdzl
 
 	public static void main(String[] args) 
 	{
+		System.out.println("Hello World1");
 		System.out.println("Hello World");
 		System.out.println("C'est iHyperMusic!");
 		
